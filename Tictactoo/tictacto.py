9492dc36dd26787595518d0b32f8a3c98a38f2c8
bhujelUpesh <115999093+bhujelupesh@users.noreply.github.com>
2024-07-16 01:52:58 -0700

    Update tictacto.py

--- Tictactoo/tictacto.py
+++ Tictactoo/tictacto.py
@@ -9,20 +9,45 @@ def print_board(board):
             print("|",board[i,j],"|", end=" ")
         print()
 
-print("1 = User")
-print("2 = Computer")
+print("---------------------Tic Tac Too------------------------")
+print("Welcome to the game......")
+print("Select Mode:")
+print("1. Single Player...")
+print("2. Two Player...")
+choice = int(input("Enter your choice : "))
 
-print(print_board(board))
-for i in range(5):
-    board = functions.user_input(board)
-    board = functions.computer_choice(board)
+if choice == 1:
+    print("1 = User")
+    print("2 = Computer")
+
     print(print_board(board))
-    if(i > 1):
-        if functions.user_wins(board):
-            print("User wins as user got 3 in a straight line")
+    for i in range(5):
+        board = functions.user_input(board)
+        board = functions.computer_choice(board)
+        print(print_board(board))
+        if(i > 1):
+            if functions.user_wins(board):
+                print("User wins as user got 3 in a straight line")
+                break
+            elif functions.computer_wins(board):
+                print("Compute Wins as computer got 3 in a straight line first")
+                break
+            else:
+                continue
+
+elif choice == 2:
+    print("1 = Player 1")
+    print("2 = Player 2")
+    
+    print(print_board(board))
+    for i in range(5):
+        board = functions.user_input(board)
+        print_board(board)
+        x = functions.win_checker(board)
+        if x == 1:
             break
-        elif functions.computer_wins(board):
-            print("Compute Wins as computer got 3 in a straight line first")
+        board = functions.user_input2(board)
+        print(print_board(board))
+        x = functions.win_checker(board)
+        if x == 1:
             break
-        else:
-            continue
