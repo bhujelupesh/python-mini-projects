9735dc1b53497bb8b218ac4644e597b03275b0c3
bhujelUpesh <115999093+bhujelupesh@users.noreply.github.com>
2024-07-16 01:52:24 -0700

    Update functions.py

--- Tictactoo/functions.py
+++ Tictactoo/functions.py
@@ -2,11 +2,24 @@ import numpy as np
 import random 
 
 def user_input(board):
+    print("Player 1")
     row = int(input("Enter the row value (0-2): "))
     column = int(input("Enter the column value (0-2): "))
     check_value = board[row,column]
     if check_value == 0:
-        board[row,column] = 1
+        board[row,column] = 'x'
+    else:
+        print("Already filled")
+        user_input(board)
+    return board
+
+def user_input2(board):
+    print("Player 2")
+    row = int(input("Enter the row value (0-2): "))
+    column = int(input("Enter the column value (0-2): "))
+    check_value = board[row,column]
+    if check_value == 0:
+        board[row,column] = 'o'
     else:
         print("Already filled")
         user_input(board)
@@ -16,63 +29,73 @@ def computer_choice(board):
     row = random.randint(0,2)
     column = random.randint(0,2)
     if board[row,column] == 0:
-        board[row,column] = 2
+        board[row,column] = 'o'
     else:
         computer_choice(board)
     return board
 
 def computer_wins(board):
-    if board[0,0] == board[0,1] == board[0,2] == 2:
+    if board[0,0] == board[0,1] == board[0,2] == 'o':
         return True
 
-    elif board[1,0] == board[1,1] == board[1,2] == 2:
+    elif board[1,0] == board[1,1] == board[1,2] == 'o':
         return True  
       
-    elif board[2,0] == board[2,1] == board[2,2] == 2:
+    elif board[2,0] == board[2,1] == board[2,2] == 'o':
         return True
 
-    elif board[0,0] == board[1,0] == board[2,0] == 2:
+    elif board[0,0] == board[1,0] == board[2,0] == 'o':
         return True
 
-    elif board[0,1] == board[1,1] == board[2,1] == 2:
+    elif board[0,1] == board[1,1] == board[2,1] == 'o':
         return True
 
-    elif board[0,2] == board[1,2] == board[2,2] == 2:
+    elif board[0,2] == board[1,2] == board[2,2] == 'o':
         return True
     
-    elif board[0,0] == board[1,1] == board[2,2] == 2:
+    elif board[0,0] == board[1,1] == board[2,2] == 'o':
         return True
 
-    elif board[0,2] == board[1,1] == board[2,0] == 2:
+    elif board[0,2] == board[1,1] == board[2,0] == 'o':
         return True
 
     else:
         return False
 
 def user_wins(board):
-    if board[0,0] == board[0,1] == board[0,2] == 1:
+    if board[0,0] == board[0,1] == board[0,2] == 'x':
         return True
 
-    elif board[1,0] == board[1,1] == board[1,2] == 1:
+    elif board[1,0] == board[1,1] == board[1,2] == 'x':
         return True
     
-    elif board[2,0] == board[2,1] == board[2,2] == 1:
+    elif board[2,0] == board[2,1] == board[2,2] == 'x':
         return True
 
-    elif board[0,0] == board[1,0] == board[2,0] == 1:
+    elif board[0,0] == board[1,0] == board[2,0] == 'x':
         return True
 
-    elif board[0,1] == board[1,1] == board[2,1] == 1:
+    elif board[0,1] == board[1,1] == board[2,1] == 'x':
         return True
 
-    elif board[0,2] == board[1,2] == board[2,2] == 1:
+    elif board[0,2] == board[1,2] == board[2,2] == 'x':
         return True
     
-    elif board[0,0] == board[1,1] == board[2,2] == 1:
+    elif board[0,0] == board[1,1] == board[2,2] == 'x':
         return True
 
-    elif board[0,2] == board[1,1] == board[2,0] == 1:
+    elif board[0,2] == board[1,1] == board[2,0] == 'x':
         return True
 
     else:
         return False
+    
+def win_checker(board):
+        if user_wins(board):
+            print("Player1 wins as Player1 got 3 in a straight line")
+            return 1
+        elif computer_wins(board):
+            print("Player2 Wins as Player2 got 3 in a straight line first")
+            return 1
+        else: 
+            return 
